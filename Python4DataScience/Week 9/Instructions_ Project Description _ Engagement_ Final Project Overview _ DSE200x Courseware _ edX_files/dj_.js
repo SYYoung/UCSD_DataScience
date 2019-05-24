97436c2dcacd3bdbb5af1f6a95c843622211806f
r38411 <r38411@yahoo.com>
2019-05-24 21:21:45 +0800

    add week 9

---
+++ Python4DataScience/Week 9/Instructions_ Project Description _ Engagement_ Final Project Overview _ DSE200x Courseware _ edX_files/dj_.js
@@ -0,0 +1,8 @@
+// Qualaroo for edx.org
+// (C) 2019 Qualaroo. All rights reserved.
+// qualaroo.com
+
+//$ site: 54527, generated: 2019-04-26 08:47:15 UTC
+//$ client: 2.0.55
+
+!function(){var e=document,t=e.getElementsByTagName("script")[0],r=e.createElement("script");r.type="text/javascript",r.async=!0,r.src="https://cl.qualaroo.com/ki.js/59450/dj_qoo.js",t.parentNode.insertBefore(r,t)}();
